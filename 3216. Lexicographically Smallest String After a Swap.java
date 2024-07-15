d2d27c46449c5ff336f5dd9a4136e544deded91f
Payal Agrawal <121183165+Payal0202@users.noreply.github.com>
2024-07-15 10:56:24 +0530

    Weekly Contest 406(Easy problem)

--- 3216. Lexicographically Smallest String After a Swap.java
+++ 3216. Lexicographically Smallest String After a Swap.java
@@ -1,3 +1,29 @@
+//Approach 2
+class Solution {
+     public String getSmallestString(String s) {
+        char[] ch = s.toCharArray();
+        for(int i=1;i<ch.length;i++){
+            int left = ch[i-1]-'0';
+            int right = ch[i]-'0';
+            if(left%2 == right%2 && left>right){
+                //swap
+                char temp=ch[i-1];
+                ch[i-1]=ch[i];
+                ch[i]=temp;
+                break;
+            }
+        }
+        s="";
+        for(char c:ch){
+            s+=c;
+        }
+
+        return s;
+    }
+}
+
+
+//Approach 1
 class Solution {
     public String getSmallestString(String s) {
         char[] ch = s.toCharArray(); // Convert the string to a character array for easy manipulation
